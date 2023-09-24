9c18f1588325d38597beeac6b9029fbb6a3c0e3e
Fernando Cortese <cortesefernando@gmail.com>
2023-09-24 14:59:39 -0300

    reintegros iguales a compras

--- src/interfaces/reintegro/schema.ts
+++ src/interfaces/reintegro/schema.ts
@@ -1,7 +1,6 @@
 import { MetodoPago } from "../../auxiliares";
-import { ICompra } from "../compra";
-import { ICompraVentaDolar } from "../compra-venta-dolar";
-import { IResumenTarjeta } from "../resumen-tarjeta";
+import { ITipoBien, ITipoCompra } from "../compra";
+import { IGrupo } from "../grupo";
 import { ITarjeta } from "../tarjeta";
 import { IUsuario } from "../usuario";
 
@@ -9,30 +8,48 @@ export interface IReintegro {
   _id?: string;
   idUsuario?: string;
 
+  // Fecha
   fecha?: string;
   periodo?: string;
+  año?: number;
+
+  // Tipo de compra
+  tipo?: ITipoCompra;
+
+  // Datos del producto
+  tipoBien?: ITipoBien;
+  descripcion?: string;
+  vendedor?: string;
+  categoria?: string;
+
+  // Datos del pago
   metodoPago?: MetodoPago;
-  importe?: number;
-  importeUSD?: number;
+  importeTotal?: number;
+  importeTotalUSD?: number;
 
-  detalle?: string;
-  idResumenTarjeta?: string;
-  idCompra?: string;
-  idCompraVentaDolar?: string;
-
-  // Tarjeta de Crédito
+  // Tarjeta de Crédito cuando el método de pago es Tarjeta de Crédito o debito automatico en tarjeta
   idTarjeta?: string;
   cuotas?: number;
   periodosPago?: string[];
   importeCuota?: number;
   importeCuotaUSD?: number;
+  importeContado?: number;
+  importeContadoUSD?: number;
+  interesAnual?: number;
+
+  // Datos de compra en comun
+  idsUsuarios?: string[];
+  idGrupo?: string;
+
+  // Datos de compra a tercero
+  idUsuarioTercero?: string;
 
   // Virtuals
   virtuals?: {
     usuario?: IUsuario;
     tarjeta?: ITarjeta;
-    compra?: ICompra;
-    compraVentaDolar?: ICompraVentaDolar;
-    resumenTarjeta?: IResumenTarjeta;
+    usuarios?: IUsuario[];
+    grupo?: IGrupo;
+    usuarioTerero?: IUsuario;
   };
 }
