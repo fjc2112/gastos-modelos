4bcc740551c6dd55e85f2dce1aa2d49799059fa3
Fernando Cortese <cortesefernando@gmail.com>
2023-09-23 23:45:52 -0300

    resumen

---
+++ src/interfaces/resumen-compras-compartidas/detalle.ts
@@ -0,0 +1,10 @@
+import { ICompra } from "../compra";
+import { IReintegro } from "../reintegro";
+import { IResumenCompraCompartida } from "./schema";
+
+export interface IDetallesResumenCompraCompartida {
+  resumen?: IResumenCompraCompartida;
+  compras1Pago?: ICompra[];
+  comprasCuotas?: ICompra[];
+  reintegros?: IReintegro[];
+}
--- src/interfaces/resumen-compras-compartidas/index.ts
+++ src/interfaces/resumen-compras-compartidas/index.ts
@@ -1,3 +1,4 @@
 export * from "./schema";
 export * from "./create";
 export * from "./update";
+export * from "./detalle";
