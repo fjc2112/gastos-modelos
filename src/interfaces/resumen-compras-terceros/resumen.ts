055bf6dbc1ce4729676c40246d0fe017f97b769b
Fernando Cortese <cortesefernando@gmail.com>
2023-11-02 11:10:44 -0300

    resumenes

--- src/interfaces/resumen-compras-terceros/resumen.ts
+++ src/interfaces/resumen-compras-terceros/resumen.ts
@@ -4,10 +4,34 @@ export interface IResumenResumenCompraTercero {
     periodoPago: string;
     sumaImportes: number;
     sumaImportesUSD: number;
+    compras1Pago: number;
+    compras1PagoUSD: number;
+    comprasCuotas: number;
+    comprasCuotasUSD: number;
+    debitosAutomaticos: number;
+    debitosAutomaticosUSD: number;
+    reintegros: number;
+    reintegrosUSD: number;
   }[];
   sumaImportes: number;
   sumaImportesUSD: number;
+  compras1Pago: number;
+  compras1PagoUSD: number;
+  comprasCuotas: number;
+  comprasCuotasUSD: number;
+  debitosAutomaticos: number;
+  debitosAutomaticosUSD: number;
+  reintegros: number;
+  reintegrosUSD: number;
   //
   promedioImportes: number;
   promedioImportesUSD: number;
+  promedioCompras1Pago: number;
+  promedioCompras1PagoUSD: number;
+  promedioComprasCuotas: number;
+  promedioComprasCuotasUSD: number;
+  promedioDebitosAutomaticos: number;
+  promedioDebitosAutomaticosUSD: number;
+  promedioReintegros: number;
+  promedioReintegrosUSD: number;
 }
